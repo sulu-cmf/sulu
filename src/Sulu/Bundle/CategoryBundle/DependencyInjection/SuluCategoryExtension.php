5514afbd10d0c7c85e04fb870b7c142fc0b84248
Alexander Schranz <alexander@sulu.io>
2024-11-26 11:48:12 +0100

    Fix npm lint by exclude content and article bundle test setup (#7683)

--- src/Sulu/Bundle/CategoryBundle/DependencyInjection/SuluCategoryExtension.php
+++ src/Sulu/Bundle/CategoryBundle/DependencyInjection/SuluCategoryExtension.php
@@ -48,7 +48,6 @@ class SuluCategoryExtension extends Extension implements PrependExtensionInterfa
             $loader->load('services_trash.xml');
         }
 
-
         if (\array_key_exists('SuluContentBundle', $bundles)) {
             $loader->load('services_content.xml');
         }
